bbd4e865e5e34022a81a3e896262f91c229dd8f1
fly <Benylaranjinha@gmail.com>
2025-11-23 18:09:42 +0000

    Creating and using a state class, and patrolling the perimeter and Chasing the player

---
+++ Assets/Scripts/GameEnviroment.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public sealed class GameEnviroment
+{
+    private static GameEnviroment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+
+    public static GameEnviroment Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameEnviroment();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+
+                instance.checkpoints = instance.checkpoints.OrderBy(waypoint => waypoint.name).ToList();
+            }
+
+            return instance;
+        }
+    }
+}
